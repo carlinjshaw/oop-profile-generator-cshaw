328c23876228810baf704bcc7a99d9bba936e4e2
carlinjshaw <carlinjshaw@gmail.com>
2021-06-28 23:33:46 -0400

    installed inquirer; got cards from bootstrap w basic html

---
+++ index.js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer');
+//idk if this works
+const template = require('html-template.js')
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
